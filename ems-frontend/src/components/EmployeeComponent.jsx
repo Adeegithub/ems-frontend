5e34b553a2e1da1071a0ee00934e364fc77cddfd
Adeesha Gunawardana <arunodagunawardana@gmail.com>
2024-05-30 22:13:36 +0530

    Connected with the backend create employee

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
+import { createEmployee } from '../services/EmployeeService'
+import { useNavigate } from "react-router-dom";
 
 const EmployeeComponent = () => {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
+
+    const navigator = useNavigate();
 
     function handleFirstName(e) {
         setFirstName(e.target.value);
@@ -19,8 +23,14 @@ const EmployeeComponent = () => {
 
     function saveEmployee(e) {
         e.preventDefault();
-        const employee = {firstName,lastName,email}
+
+        const employee = { firstName, lastName, email }
         console.log(employee)
+
+        createEmployee(employee).then((response) => {
+            console.log(response.data)
+            navigator('/employees')
+        })
     }
 
 
