16206ae1fde67875c4dc401053b209a92510ff81
Adeesha Gunawardana <arunodagunawardana@gmail.com>
2024-05-30 19:26:01 +0530

    Added AddEmployee Component and Configured the Route

---
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmployeeComponent = () => {
+    return (
+        <div>
+            This is Button Testing
+        </div>
+    )
+}
+
+export default EmployeeComponent
